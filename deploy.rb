81f6c3005f112cb759f5fb55c913697028aa1fd1
Takashi Kokubun <takashi-kokubun@cookpad.com>
2014-09-27 12:34:23 +0900

    fix

--- deploy.rb
+++ deploy.rb
@@ -3,5 +3,5 @@ git "/home/isucon/webapp" do
   user "isucon"
 end
 
-execute("cd /home/isucon/webapp/ruby; /home/isu-user/isucon2-ruby/config/env.sh bundle install")
+execute("cd /home/isucon/webapp/ruby; /home/isucon/env.sh bundle install")
 execute("supervisorctl restart isucon_ruby")
